f7a601618d11be97a4cff6eb6a06d0aafdecd05e
Piotr Filipiak <kiddhkane@gmail.com>
2023-10-05 23:13:29 +0200

    Added zod. You can add and edit tasks.

--- app/_components/TodoList.tsx
+++ app/_components/TodoList.tsx
@@ -1,16 +1,56 @@
 "use client";
 
+import { FormEvent, useState } from "react";
 import { trpc } from "../_trpc/client";
 
 const TodoList = () => {
   const getTodos = trpc.getTodos.useQuery();
+  const addTodo = trpc.addTodo.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    },
+  });
+  const setDone = trpc.setDone.useMutation({
+    onSettled: () => {
+      getTodos.refetch();
+    },
+  });
+
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Submitting: ", taskTitle);
+    addTodo.mutate(taskTitle);
+    setTaskTitle("");
+  };
+
   return (
-    <div>
-      {/* {JSON.stringify(getTodos.data)} */}
-      {getTodos.data?.map((todo, idx) => (
-        <p key={idx}>Todo: {todo}</p>
-      ))}
-    </div>
+    <>
+      <div>
+        {/* {JSON.stringify(getTodos.data)} */}
+        {getTodos.data?.map((todo, idx) => (
+          <div key={idx} className="flex justify-start items-center">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={async () => {
+                setDone.mutate({ id: todo.id, done: !todo.completed });
+              }}
+            />
+            <p>{todo.title}</p>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          value={taskTitle}
+          onChange={(e) => setTaskTitle(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
+    </>
   );
 };
 
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,31 @@
+import { usePrisma } from "@/lib/hooks/usePrisma";
 import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
 
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30];
+    return await usePrisma.task.findMany({ orderBy: { id: "asc" } });
   }),
+  addTodo: publicProcedure.input(z.string()).mutation(async (options) => {
+    const addedTask = await usePrisma.task.create({
+      data: { title: options.input },
+    });
+    return addedTask;
+  }),
+  setDone: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        done: z.boolean(),
+      })
+    )
+    .mutation(async (options) => {
+      const updatedTask = await usePrisma.task.update({
+        where: { id: options.input.id },
+        data: { completed: options.input.done },
+      });
+      return updatedTask;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
